5e072d72bd9d7096ce196fc646728fb9da7828d9
TharinduPramodya <86798214+TharinduPramodya@users.noreply.github.com>
2024-04-28 22:12:12 +0530

    Rem

--- cafeteria_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include"database.php";
-	session_start();
-	$s="delete from cafe where caid={$_GET["id"]}";
-	$db->query($s);
-	echo"<script>window.open('cafeteria.php','_self');</script>";
-
-?>

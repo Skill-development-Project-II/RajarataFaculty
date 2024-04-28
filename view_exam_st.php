c7bdb96e6d14a8e3e4df64fab95449ed19be3a9b
Lahiru Sandaranga <107667592+lahirusadaranga@users.noreply.github.com>
2024-04-29 01:11:03 +0530

    Student

---
+++ view_exam_st.php
@@ -0,0 +1,183 @@
+<?php
+	include"database.php";
+	session_start();
+	if (isset($_SESSION["ID"])){
+		$logged_user = $_SESSION["NAME"];
+	} elseif (isset($_SESSION["NID"])) {
+		$logged_user = $_SESSION["NNAME"];
+	} else {
+		echo"<script>window.open('index.php?mes=Access Denied...','_self');</script>";
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>View Exam Time Table</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+		<style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        th {
+            background-color: #343a40;
+            color: white;
+        }
+    </style>
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php
+            include('sidebar2.php');
+        ?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+						<?php include "navbar.php";?>
+            <div id="content">
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+					
+						<h3 style="color: black;" class = "py-3">View Exam Time Table Details</h3><br>
+						
+						<?php
+							if(isset($_GET["mes"]))
+								{
+									echo"<div class='error'>{$_GET["mes"]}</div>";	
+								}
+					
+						?>
+						
+						<table border="1px">
+							<tr>
+								<th>S.No</th>
+								<th>Class </th>
+								<th>Subject</th>
+								<th>Exam Name</th>
+								<th>Term</th>
+								<th>Date</th>
+								<th>Session</th>
+								<th>Update</th>
+								<th>Delete</th>
+								
+							</tr>
+							<?php
+								$s="select * from exam";
+								$res=$db->query($s);
+								if($res->num_rows>0)
+								{
+									$i=0;
+									while($r=$res->fetch_assoc())
+									{
+										$i++;
+										echo "
+											<tr>
+												<td>{$i}</td>
+												<td>{$r["CLASS"]}</td>
+												<td>{$r["SUB"]}</td>
+												<td>{$r["ENAME"]}</td>
+												<td>{$r["ETYPE"]}</td>
+												<td>{$r["EDATE"]}</td>
+												<td>{$r["SESSION"]}</td>
+												<td><a href='exam_update.php?id={$r["EID"]}' style='color: white; background-color: blue;' class='btnr'>Update</a></td>
+												<td><a href='exam_delete.php?id={$r["EID"]}' style='color: white; background-color: red;' class='btnr'>Delete</a></td>
+											</tr>
+										
+										
+										
+										";
+										
+									}
+								}
+								else
+								{
+									echo "No Record Found";
+								}
+							
+							
+							
+							?>
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						</table>
+				
+				</div>
+				
+				
+			</div>
+	
+			
+
+			<br><br>
+		<script src="js/jquery.js"></script>
+		<script>
+			$(document).ready(function(){
+				$(".error").fadeTo(1000, 100).slideUp(1000, function(){
+						$(".error").slideUp(1000);
+				});
+				
+				$(".success").fadeTo(1000, 100).slideUp(1000, function(){
+						$(".success").slideUp(1000);
+				});
+			});
+		</script>
+		<!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
